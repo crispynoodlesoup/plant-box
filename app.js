f1c1db37030a22bfd5c663b9738ff63d74aa42f5
crispynoodlesoup <thepanman321@gmail.com>
2023-02-13 12:17:52 -0600

    fix like buttons

--- app.js
+++ app.js
@@ -23,18 +23,15 @@ bell.addEventListener("click", (e) => {
     }
 });
 
-let likeSwitch = false;
-
 const likeList = document.querySelectorAll(".like");
 
 likeList.forEach(like => like.addEventListener("click", (e) => {
-    if (likeSwitch) {
+    if (like.classList.contains("like-active")) {
         like.classList.add("like-animate2");
         setTimeout(() => {
             like.classList.remove("like-animate2");
             like.classList.remove("like-active")
             like.src = "./Assets/heart-outline.svg";
-            likeSwitch = false;
         }, 150);
     } else {
         like.classList.add("like-animate");
@@ -42,23 +39,19 @@ likeList.forEach(like => like.addEventListener("click", (e) => {
             like.classList.remove("like-animate");
             like.classList.add("like-active");
             like.src = "./Assets/heart.svg";
-            likeSwitch = true;
         }, 150);
     }
 }));
 
-let collabSwitch = false;
-
 const collabList = document.querySelectorAll(".collab");
 
 collabList.forEach(collab => collab.addEventListener("click", (e) => {
-    if (collabSwitch) {
+    if (collab.classList.contains("collab-active")) {
         collab.classList.add("collab-animate2");
         setTimeout(() => {
             collab.classList.remove("collab-animate2");
             collab.classList.remove("collab-active");
             collab.src = "./Assets/account.svg";
-            collabSwitch = false;
         }, 150);
     }
     else {
@@ -67,7 +60,6 @@ collabList.forEach(collab => collab.addEventListener("click", (e) => {
             collab.classList.remove("collab-animate");
             collab.classList.add("collab-active");
             collab.src = "./Assets/account-group.svg";
-            collabSwitch = true;
         }, 150);
     }
 }));
